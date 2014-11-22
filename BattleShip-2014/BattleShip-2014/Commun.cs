eb97ee55deb3c66541573b6964eefc039066e406
cheroukee <fournierje.info@gmail.com>
2014-11-22 12:09:51 -0500

    Ajout de la structure de point "sPoint", pour les occasions ou l'utilisation d'un type est nécessaire pour contenir des points X et Y (retour d'une fonction List<sPoint>.
    Ajout de l'énum de rotation, utile pour désigner l'orientation d'une pièce sur le tableau

--- BattleShip-2014/BattleShip-2014/Commun.cs
+++ BattleShip-2014/BattleShip-2014/Commun.cs
@@ -6,6 +6,45 @@ using System.Threading.Tasks;
 
 namespace BattleShip_2014
 {
+    enum Rotation
+    {
+        Haut = 0,
+        Droite = 90,
+        Bas = 180,
+        Gauche = 270
+    }
+
+    struct sPoint
+    {
+        private int x_;
+        public int X
+        {
+            get
+            {
+                return x_;
+            }
+            set
+            {
+                if (value < 100 && value >= 0)
+                    x_ = value;
+            }
+        }
+
+        private int y_;
+        public int Y
+        {
+            get
+            {
+                return y_;
+            }
+            set
+            {
+                if (value < 100 && value >= 0)
+                    y_ = value;
+            }
+        }
+    }
+
     public class Commun
     {
     }
