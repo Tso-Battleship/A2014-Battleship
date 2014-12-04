a94b4e76c0d0328f4870a7ab0563a4700aca6ef9
kyruos <daviduchesne@gmail.com>
2014-12-04 16:04:13 -0500

    Changement de form pour test avec client (2)

---
+++ BattleShip-2014 - V1/BattleShip-2014/TCPServeur.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+
+namespace BattleShip_2014
+{
+    class TCPServeur
+    {
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private string[] adrIp = { "" };
+        public string[] strMessage = new string[4];
+
+
+        public TCPServeur()
+        {
+            this.tcpListener = new TcpListener(IPAddress.Any, 3000);            //Crée un socket avec 127.0.0.1 et le port 3000.
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));  //Crée un nouveau thread qui écoute pour des clients
+            this.listenThread.Start();
+            adrIp = new string[4];
+
+        }
+        /// <summary>
+        /// Delegate qui attends de nouveaux clients. Il crée un nouveau thread lorsqu'un client se connecte.
+        /// </summary>
+        private void ListenForClients()
+        {
+            this.tcpListener.Start();
+            int nbClients = 0;
+            while (true)
+            {
+                //Code bloquant jusqu'à ce qu'un client soit accepté
+                TcpClient client = this.tcpListener.AcceptTcpClient();
+                //Crée un thread qui va gérer la communication avec le client
+                Thread clientThread = new Thread(() => HandleClientComm(client, nbClients));
+                clientThread.Start();
+                /*chagnement dans le code ici*/
+                //**************************************
+                //while (adrIp[nbClients] == "") ;
+                //IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(adrIp[nbClients]), 3000);
+                //client.Connect(serverEndPoint);
+                // NetworkStream clientStream = client.GetStream();
+                // ASCIIEncoding encoder = new ASCIIEncoding();
+                nbClients++;
+                //***************************************
+            }
+        }
+
+        private void HandleClientComm(object client, int numClient)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
+                strMessage[numClient] = encoder.GetString(message, 0, bytesRead);
+            }
+
+            tcpClient.Close();
+        }
+        public void envoyerCommande(object client, string message)
+        {
+            byte[] buffer = new byte[4096];
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            buffer = encoder.GetBytes(message);
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+        }
+    }
+}
---
+++ BattleShip-2014 - V1/ClientBattleShip/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClientBattleShip;
+using System.Net.Sockets;
+using System.Net;
+
+namespace BattleShip_2014
+{
+    public partial class Client : Form
+    {
+        TCPClient tcpClient = new TCPClient();
+        TcpClient client = new TcpClient();
+ 
+        public Client()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void connecterServeur_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+                client.Connect(serverEndPoint);
+                NetworkStream clientStream = client.GetStream();
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                tcpClient.envoyerCommande(client, tcpClient.retourneAdresseIpClient());
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                MessageBox.Show("La connection au serveur a été refusé");
+            }
+            
+           // textBox3.Text = TCPClient.retourneAdresseIpClient();
+        }
+
+    
+    }
+}
