7f4cfabc281417ca039a36aaa3a9ca6623408273
glights <tigui.03@gmail.com>
2014-12-04 13:17:21 -0500

    création des classes TableauAvecPiece et TableauDeJeu

--- BattleShip-2014/BattleShip-2014/TableauAvecPiece.cs
+++ BattleShip-2014/BattleShip-2014/TableauAvecPiece.cs
@@ -8,5 +8,17 @@ namespace BattleShip_2014
 {
     public class TableauAvecPiece : TableauDeJeu
     {
+        public void TableauAvecPieces(int tailleX, int tailleY,list<>)
+	    {
+		    
+	    }
+	    public void TableauAvecPieces(TableauDeJeu Base,list<>)
+	    {
+		    Base.cases_[1][2] = 1;
+	    }
+	    public bool piecesToutesTouchees()
+	    {
+		    return true;
+	    }
     }
 }
--- BattleShip-2014/BattleShip-2014/TableauDeJeu.cs
+++ BattleShip-2014/BattleShip-2014/TableauDeJeu.cs
@@ -8,5 +8,31 @@ namespace BattleShip_2014
 {
     public class TableauDeJeu
     {
+        public int[][] cases_;
+        int tailleX_, tailleY_;
+
+        /**
+         * methode pour créer le tableau de jeu
+         * */
+        public void TableauDeJeu(int tailleX, int tailleY)
+	    {
+		    cases_ = new int[tailleX][tailleY];
+		    tailleX_ = tailleX;
+		    tailleY_ = tailleY;
+	    }
+
+        /**
+        *methode 
+         *parametre tirX: position X du tir
+         *parametre tirY: position Y du tir 
+         *retourne true si le tir est dans le tableau
+        */
+        public bool TirerSurCase(int tirX, int tirY)
+        {
+            if ((tirX <= tailleX_) | (tirY <= tailleY_))
+                return true;
+            else
+                return false;
+        }
     }
 }
