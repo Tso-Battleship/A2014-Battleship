63c5d0977401c3c2ab5117b0a1015a40f83bada7
glights <glights>
2014-12-11 17:35:43 -0500

    petit règlage
    petit règlage pour tirerPièce

--- BattleShip-2014/BattleShip-2014/TableauAvecPiece.cs
+++ BattleShip-2014/BattleShip-2014/TableauAvecPiece.cs
@@ -8,33 +8,40 @@ namespace BattleShip_2014
 {
     public class TableauAvecPiece : TableauDeJeu
     {
-
+        
         private List<Piece> pieces_;
 
+        // permet de chercher la liste et la "seter"
         public List<Piece> Pieces
         {
             get { return pieces_; }
             set { pieces_ = value; }
         }
 
-
-        // selon le forum de stackoverflow.com ca fonctionne :p
+        /* créer le tableau de jeu avec les pièces à l'intérieur
+         * 
+         * parametre: tailleX taille en X du tableau
+         * parametre: tailleX taille en X du tableau
+         * parametre: list<Piece> liste de pièces
+         * */
         public TableauAvecPiece(int tailleX, int tailleY, List<Piece> pieces) :
-            base(tailleX, tailleY)
+            base(tailleX, tailleY) 
         {
             pieces_ = pieces;
         }
 
+        // vérifie si TOUTES les pièces sont touchées
         public bool piecesToutesTouchees()
         {
             int i = 0;
-            foreach (Piece p in pieces_)
+
+            foreach (Piece p in pieces_)    // une pièces à la fois
             {
-                if (p.toutesCasesToucher())
+                if (p.toutesCasesToucher()) // case touché?
                     i++;
             }
 
-            if (i == pieces_.Count)
+            if (i == pieces_.Count) // TOUTE touché?
                 return true;
             else
                 return false;
--- BattleShip-2014/BattleShip-2014/TableauDeJeu.cs
+++ BattleShip-2014/BattleShip-2014/TableauDeJeu.cs
@@ -11,7 +11,8 @@ namespace BattleShip_2014
         protected CaseDeJeux[,] cases_;
         protected int tailleX_, tailleY_;
 
-        public CaseDeJeux[,] Cases
+        // détermine les cases de jeu
+        public CaseDeJeux[,] Cases 
         {
             get
             {
@@ -19,6 +20,7 @@ namespace BattleShip_2014
             }
         }
 
+        // détermine la taille en X du tableau de jeu
         public int TailleX
         {
             get
@@ -27,6 +29,7 @@ namespace BattleShip_2014
             }
         }
 
+        // détermine la taille en Y du tableau de jeu
         public int TailleY
         {
             get
@@ -35,16 +38,23 @@ namespace BattleShip_2014
             }
         }
 
+        /*
+         * 
+         * Fonction pour créer un tableau de jeu
+         * 
+         * parametre: détermine la taille en X du tableau de jeu
+         * parametre: détermine la taille en Y du tableau de jeu
+         * 
+         * */
         public TableauDeJeu(int tailleX, int tailleY)
 	    {
-            cases_ = new CaseDeJeux[tailleX, tailleY];
+            cases_ = new CaseDeJeux[tailleX, tailleY];  // tableau de case
 
             for (int i = 0; i < tailleX; i++)
             {
                 for(int j = 0; j < tailleY; j++)
                 {
-                    cases_[i, j] = new CaseDeJeux(i, j);
-                    
+                    cases_[i, j] = new CaseDeJeux(i, j);    // initialise et créer chaque case du tableau de cases                  
                 }
             }
                 
@@ -56,26 +66,31 @@ namespace BattleShip_2014
         *methode 
          *parametre tirX: position X du tir
          *parametre tirY: position Y du tir
-         *retourne true si le tir est dans le tableau
-        */
-        public bool tirerSurCase(int tirX, int tirY)
-        {           
-            if (((tirX < tailleX_) && (tirX >= 0)) && ((tirY < tailleY_)&&(tirY >=0)))    // verifie si le le tir est dans le tableau
+         *retourne 0 si le tire a été réussit
+         *retourne 1 si la case a déjà été touché
+         *retourne 2 la case est à l'exterieur du tableau
+         **/
+        public int tirerSurCase(int tirX, int tirY)
+        {   
+            // verifie si le le tir est dans le tableau
+            if (((tirX < tailleX_) && (tirX >= 0)) && 
+                ((tirY < tailleY_)&&(tirY >=0)))   
             {
-                CaseDeJeux c = cases_[tirX, tirY];
+                //vérifie si la case est touché ou pas
+                CaseDeJeux c = cases_[tirX, tirY];  
                 if (c.EstTouchee)
                 {
-                    return false;
+                    return 1;
                 }
-                else
+                else // sinon tire
                 {
-                    c.tirer();
-                    return true;
+                    c.tirer(); 
+                    return 0;
                 }
             }
             else
             {
-                return false;
+                return 2;
             }
         }
 
